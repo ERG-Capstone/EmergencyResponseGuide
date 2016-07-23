18bc1710a38dec8e581b30a25a9a1aee91feee31
Spencer A. Bywater <spencer.bywater@gmail.com>
2016-07-23 12:48:45 -0700

    Whitespace

--- tools/build.py
+++ tools/build.py
@@ -2,10 +2,12 @@ from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('base.html')
+
 args = {
     'content': 'Hello World!',
     'title': 'Sup'
 }
+
 output_from_parsed_template = template.render(args)
 
 with open('www/new.html', 'wb') as fh:
