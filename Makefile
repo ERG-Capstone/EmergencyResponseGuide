c96fbc4989bfb42c6989727527260901bbf9ddbf
Spencer A. Bywater <spencer.bywater@gmail.com>
2016-08-21 21:36:40 -0700

    Build on one test template working properly.

--- Makefile
+++ Makefile
@@ -4,3 +4,8 @@ all: build
 
 build:
 	# Line to assemble html resources goes here
+	python tools/build.py
+
+install_deps:
+	pip install -r requirements.txt
+	
--- tools/build.py
+++ tools/build.py
@@ -1,14 +1,19 @@
 from jinja2 import Environment, FileSystemLoader
+from BeautifulSoup import BeautifulSoup as bs
 
-env = Environment(loader=FileSystemLoader('templates'))
+env = Environment(loader=FileSystemLoader('templates'),
+                  extensions=['jinja2.ext.with_'])
 template = env.get_template('base.html')
 
-args = {
-    'content': 'Hello World!',
-    'title': 'Sup'
-}
+with open('src/test.html', 'r') as content_file:
+    file_contents = content_file.read()
 
-output_from_parsed_template = template.render(args)
+output_from_parsed_template = template.render(content=file_contents)
+new_template = env.from_string(output_from_parsed_template)
+final_output = new_template.render()
 
-with open('www/new.html', 'wb') as fh:
-    fh.write(output_from_parsed_template)
+soup = bs(final_output)
+pretty_final_output = soup.prettify().encode('utf-8')
+
+with open('stage/new.html', 'wb') as fh:
+    fh.write(pretty_final_output)
