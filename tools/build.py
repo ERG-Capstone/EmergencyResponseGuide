a3dd0b28149d83e8721992fea64cb835376176ae
Spencer A. Bywater <spencer.bywater@gmail.com>
2016-08-24 20:26:19 -0700

    Hopefully a complete build solution.

--- tools/build.py
+++ tools/build.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from BeautifulSoup import BeautifulSoup as bs
 from jinja2 import Environment, FileSystemLoader
@@ -12,11 +13,16 @@ working_directory = os.getcwd() + '/src'
 # Create stage directory if it does not exist
 if 'stage' not in os.listdir(os.getcwd()):
     os.mkdir('stage')
+else:
+    shutil.rmtree('stage')
+    os.mkdir('stage')
 
 # Template-ize each html in src and copy it to stage
 for i in os.listdir(working_directory):
+    srci = 'src/' + i
+    dsti = 'stage/' + i
     if i.endswith('.html'):
-        with open('src/' + i, 'r') as content_file:
+        with open(srci, 'r') as content_file:
             file_contents = content_file.read()
 
         output_from_parsed_template = template.render(content=file_contents)
@@ -26,5 +32,13 @@ for i in os.listdir(working_directory):
         soup = bs(final_output)
         pretty_final_output = soup.prettify().encode('utf-8')
 
-        with open('stage/' + i, 'wb') as fh:
+        with open(dsti, 'wb') as fh:
             fh.write(pretty_final_output)
+    else:
+        if '.' not in i:
+            try:
+                shutil.copytree(srci, dsti)
+            except Exception:
+                shutil.copy2(srci, dsti)
+        else:
+            shutil.copy2(srci, dsti)
